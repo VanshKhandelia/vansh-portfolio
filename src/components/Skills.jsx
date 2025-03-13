bddef594b3cd6be2e33db62d6996a6d43ce5d193
VanshKhandelia <khandeliavansh@gmail.com>
2025-03-13 16:42:34 -0700

    added the skills section functionaloty without the styling

---
+++ src/components/Skills.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "../assets/styles/components/_skills.scss";
+import { useState, useEffect } from "react";
+import { restBase } from "../utilities/Utilities";
+
+const Skills = () => {
+  const restPathSkills = restBase + "portfolio_skills?per_page=100";
+  const restPathSkillCategories = restBase + "skill_category";
+  const [restDataSkills, setDataSkills] = useState([]);
+  const [restDataSkillCategories, setDataSkillCategories] = useState([]);
+  const [isLoaded, setLoadStatus] = useState(false);
+  const [activeCategory, setActiveCategory] = useState("all");
+  const [filteredSkills, setFilteredSkills] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const responseSkills = await fetch(restPathSkills);
+      const responseCategories = await fetch(restPathSkillCategories);
+      if (responseSkills.ok && responseCategories.ok) {
+        const dataSkills = await responseSkills.json();
+        const dataCategories = await responseCategories.json();
+        setDataSkills(dataSkills);
+        setDataSkillCategories(dataCategories);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
+    };
+    fetchData();
+    if (activeCategory === "all") {
+      setFilteredSkills(restDataSkills);
+    }
+  }, [restPathSkillCategories, restDataSkills]);
+
+  const filterSkills = (category) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setFilteredSkills(restDataSkills);
+    } else {
+      setFilteredSkills(
+        restDataSkills.filter(
+          (skill) =>
+            skill.skill_category && skill.skill_category.includes(category)
+        )
+      );
+    }
+  };
+
+  return (
+    <div>
+      <h2 className="section-heading">Skills</h2>
+      <div className="Buttons">
+        <button
+          className={activeCategory === "all" ? "active" : ""}
+          onClick={() => filterSkills("all")}
+        >
+          All
+        </button>
+        {restDataSkillCategories.map((category) => (
+          <button
+            key={category.id}
+            className={activeCategory === category.id ? "active" : ""}
+            onClick={() => filterSkills(category.id)}
+          >
+            {category.name}
+          </button>
+        ))}
+      </div>
+      <ul className="list-disc pl-5">
+        {filteredSkills.map((skill) => (
+          <li key={skill.id}>{skill.title.rendered}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -4,12 +4,14 @@ import About from "../components/About";
 import ProjectGallery from "../components/ProjectGallery";
 import Contact from "../components/Contact";
 import Navigation from "../components/Navigation";
+import Skills from "../components/Skills";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <ProjectGallery />
+      <Skills />
       <About />
       <Contact />
     </div>
